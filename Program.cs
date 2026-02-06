d2734421f2546ca1065f33385998c09c63cb1917
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-02-06 00:31:21 -0600

    fix: resolve merge conflicts update for Telegram library

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ using NotificationsTelegram.Hub;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
